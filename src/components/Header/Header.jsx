1303af5f222f1a40447e6e36e412b4c918ccea20
dedecanton <andrecanton18@gmail.com>
2021-11-23 17:23:01 -0300

    change logo and user ref & scroll smoth

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,13 +5,13 @@ export default ({black}) => {
     return(
         <header className={black ? 'black' : ''}>
             <div className="header--logo">
-                <a href="/">
+                <a href="#">
                     <img src="https://logodownload.org/wp-content/uploads/2014/10/netflix-logo-5.png" alt="Netflix" />
                 </a>
             </div>
 
             <div className="header--user">
-                <a href="/">
+                <a href="#">
                     <img src="https://i.pinimg.com/originals/b6/77/cd/b677cd1cde292f261166533d6fe75872.png" alt="Usuário logado" />
                 </a>
             </div>
